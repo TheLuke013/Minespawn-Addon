e7fc7773d9e0e08dd5b788f183bf4d85f583b993
TheLuke013 <araujochagaslucas2@gmail.com>
2024-12-28 16:40:50 -0300

    Added weapons custom components

---
+++ BP/scripts/main.js
@@ -0,0 +1,28 @@
+import { world } from '@minecraft/server';
+
+world.beforeEvents.worldInitialize.subscribe(initEvent => {
+    initEvent.itemComponentRegistry.registerCustomComponent('minespawn:knockback', {
+        onHitEntity: e => {
+            const dir = e.attackingEntity.getViewDirection();
+            e.hitEntity.applyKnockback(dir.x + 20, dir.y + 20, dir.z + 20, 1);
+        }
+    });
+
+    initEvent.itemComponentRegistry.registerCustomComponent('minespawn:flaming_damage', {
+        onHitEntity: e => {
+            e.hitEntity.setOnFire(10, false);
+        }
+    });
+
+    initEvent.itemComponentRegistry.registerCustomComponent('minespawn:weapon_damage', {
+        onHitEntity: e => {
+            const weapon = e.itemStack;
+
+            if (weapon.typeId === 'minespawn:big_bertha' && 'minespawn:slice') {
+                e.hitEntity.applyDamage(500);
+            } else if (weapon.typeId === 'minespawn:royal_guardian') {
+                e.hitEntity.applyDamage(750);
+            }
+        }
+    });
+});
