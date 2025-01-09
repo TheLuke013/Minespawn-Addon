935f2866697941592c08894edec40d2193da3fbf
TheLuke013 <araujochagaslucas2@gmail.com>
2025-01-09 16:17:04 -0300

    Updated Slice(Stelix Bertha)

--- BP/scripts/main.js
+++ BP/scripts/main.js
@@ -1,16 +1,33 @@
-import { world } from '@minecraft/server';
+import { world, system, EnchantmentType } from '@minecraft/server';
+
+function getPlayerMainhandItem(player) {
+    const equippable = player.getComponent('equippable');
+    return equippable.getEquipment('Mainhand');
+}
 
 world.beforeEvents.worldInitialize.subscribe(initEvent => {
     initEvent.itemComponentRegistry.registerCustomComponent('minespawn:knockback', {
         onHitEntity: e => {
             const dir = e.attackingEntity.getViewDirection();
-            e.hitEntity.applyKnockback(dir.x, dir.z, 10, 1);
+            const weapon = e.itemStack;
+
+            if (weapon.typeId === 'minespawn:stelix_bertha') {
+                e.hitEntity.applyKnockback(dir.x, dir.z, 30, 1);
+            } else {
+                e.hitEntity.applyKnockback(dir.x, dir.z, 10, 1);
+            }
         }
     });
 
     initEvent.itemComponentRegistry.registerCustomComponent('minespawn:flaming_damage', {
         onHitEntity: e => {
-            e.hitEntity.setOnFire(10, false);
+            const weapon = e.itemStack;
+
+            if (weapon.typeId === 'minespawn:stelix_bertha') {
+                e.hitEntity.setOnFire(30, false);
+            } else {
+                e.hitEntity.setOnFire(10, false);
+            }
         }
     });
 
@@ -18,9 +35,10 @@ world.beforeEvents.worldInitialize.subscribe(initEvent => {
         onHitEntity: e => {
             const weapon = e.itemStack;
 
-            if (weapon.typeId === 'minespawn:big_bertha' || weapon.typeId === 'minespawn:slice') {
-                world.sendMessage("Big ou Slice");
+            if (weapon.typeId === 'minespawn:big_bertha') {
                 e.hitEntity.applyDamage(500);
+            } else if (weapon.typeId === 'minespawn:stelix_bertha') {
+                e.hitEntity.applyDamage(580);
             } else if (weapon.typeId === 'minespawn:royal_guardian') {
                 e.hitEntity.applyDamage(750);
             }
